76d3fe65008f3f2743b89ccb6e34bf5cab2f0483
Dorota Pawłowska <dpawlowska@op.pl>
2017-11-09 18:34:05 +0100

    commit nr17.2 JavaScript kurs1

--- test.js
+++ test.js
@@ -1,7 +1,21 @@
-var linki = document.getElementsByTagName("a");
+/*var linki = document.getElementsByTagName("a");
 
 for(var i = 0; i < linki.length; i++){
 
     linki[i].className = "link-" + i;
 
 }
+*/
+(function IIFE(){
+    console.log( "Hello!" );
+})();
+// "Hello!"
+
+var a = 42;
+
+(function IIFE(){
+    var a = 10;
+    console.log( a );	// 10
+})();
+
+console.log( a );
