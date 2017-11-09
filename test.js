79f9bdc0f4d4d7fe53e338e7ec64d2176bc84991
Dorota Pawłowska <dpawlowska@op.pl>
2017-11-09 15:28:15 +0100

    commit nr10 JavaScript kurs1

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 var wiek = 44;
 
-if(wiek > 30){
+if(wiek == 30){
     document.write("jesteś po 30tce.");
 }else if(wiek > 20){
     document.write("jesteś po 20tce.");
-}else if(wiek > 10){
+}else if(wiek >= 10){
     document.write("jesteś po 10tce.");
 }else{
     document.write("nie jesteś nawet po 10tce.");
