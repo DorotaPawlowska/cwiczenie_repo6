27f2784c7c777fad710805bbff177ce4c72c4987
Dorota Pawłowska <dpawlowska@op.pl>
2017-11-09 20:58:27 +0100

    commit nr20 JavaScript kurs1

--- test.js
+++ test.js
@@ -1,14 +1,11 @@
-var srednia = 0;
-function Srednia( a, b){
-    srednia = (a + b )/2;//zmienna globalna
-    console.log(srednia);
-    return srednia;
-}
+var a = 7;
+var b = 5;
 
-var mojaSrednia = Srednia(2,4);// zmienna globalna
+console.log(a+b);
+console.log(typeof(a+b));
 
-function logResult() {
-    console.log("moja średnia " + mojaSrednia + " w funkcji");
-}
-
-logResult();
+console.log(Math.round(7.8));
+console.log(Math.floor(7.9));
+console.log(Math.ceil(7.2));
+console.log(Math.max(7,2,6,9,11));
+console.log(Math.PI);
