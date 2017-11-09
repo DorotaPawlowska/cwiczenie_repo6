6e635f27b6770267bc439af7343b3c402b7f6d93
Dorota Pawłowska <dpawlowska@op.pl>
2017-11-09 16:10:30 +0100

    commit nr12 JavaScript kurs1

--- test.js
+++ test.js
@@ -1,25 +1,11 @@
-var wiek = 44;
-var x = "4";
-var y = 4;
+var wiek = 26;
 
-if(wiek > 30){
-    document.write("    jesteś po 30tce.");
-}else if(wiek > 20){
-    document.write("    jesteś po 20tce.");
-}else if(wiek > 10){
-    document.write("    jesteś po 10tce.");
-}else{
-    document.write("     nie jesteś nawet po 10tce.");
+
+if(wiek < 18 || wiek > 30 || wiek === 25){
+    document.write("you aint coming!");
+}else {
+    document.write("you can come, you cool dude !!");
 }
 
-if(x == y){
-    document.write("    x = y");
-}else{
-    document.write("     x nie jest równy y");
-}
-
-if(x !== y){
-    document.write("     xx nie jest równy y z powodu typu ");
-}else{
-    document.write("     bla bla bla");
-}
+//&& i
+//|| lub
