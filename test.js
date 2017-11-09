ba481ca9a5b8a041082c4bb0e3a8f36c116d6b80
Dorota Pawłowska <dpawlowska@op.pl>
2017-11-09 21:07:45 +0100

    commit nr21 JavaScript kurs1

--- test.js
+++ test.js
@@ -1,11 +1,21 @@
-var a = 7;
+var a = 6;
 var b = 5;
 
-console.log(a+b);
-console.log(typeof(a+b));
 
+// podwujna negacja
+if( !isNaN(a)){
+    console.log("a wynik to ... " + (a * b));
+} else {
+    console.log("to nie jest numer");
+}
+
+
+//console.log(typeof(a+b));
+
+/*
 console.log(Math.round(7.8));
 console.log(Math.floor(7.9));
 console.log(Math.ceil(7.2));
 console.log(Math.max(7,2,6,9,11));
 console.log(Math.PI);
+*/
