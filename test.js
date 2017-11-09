99c52d8fe2bd5cf473eec42eedf60d05b6f7f87a
Dorota Pawłowska <dpawlowska@op.pl>
2017-11-09 20:28:02 +0100

    commit nr18 JavaScript kurs1

--- test.js
+++ test.js
@@ -1,48 +1,8 @@
-/*var linki = document.getElementsByTagName("a");
-
-for(var i = 0; i < linki.length; i++){
-
-    linki[i].className = "link-" + i;
-
-}
-*/
-(function IIFE(){
-    console.log( "Hello!" );
-})();
-// "Hello!"
-
-var a = 42;
-
-(function IIFE(){
-    var a = 10;
-    console.log( a );	// 10
-})();
-
-console.log( a );
-
-function makeAdder(x) {
-    // parameter `x` is an inner variable
-
-    // inner function `add()` uses `x`, so
-    // it has a "closure" over it
-    function add(y) {
-        return y + x;// a 'y' to co to ????
-    }
-
-    return add;
+function Srednia( a, b, c, d){
+    var srednia = (a + b + c + d)/4;
+    console.log(srednia);
+    return srednia;
 }
 
-// `plusOne` gets a reference to the inner `add(..)`
-// function with closure over the `x` parameter of
-// the outer `makeAdder(..)`
-var plusOne = makeAdder( 1 );
-
-// `plusTen` gets a reference to the inner `add(..)`
-// function with closure over the `x` parameter of
-// the outer `makeAdder(..)`
-var plusTen = makeAdder( 10 );
-
-console.log(plusOne( 3 ));		// 4  <-- 1 + 3
-console.log(plusOne( 41 ));		// 42 <-- 1 + 41
-
-console.log(plusTen( 13 ));		// 23 <-- 10 + 13
+var mojaSrednia = Srednia(2,4,5,5);
+console.log("moja średnia " + mojaSrednia);
