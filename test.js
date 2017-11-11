bf11af6fb9555326aee7626d53a0df89f01e3f00
Dorota Pawłowska <dpawlowska@op.pl>
2017-11-11 14:05:52 +0100

    commit nr24 JavaScript kurs1

--- test.js
+++ test.js
@@ -1,3 +1,36 @@
+
+//#epizod 26 kursu
+var mojaTablica = [];
+mojaTablica[0] = 25;
+console.log(mojaTablica[0]);
+mojaTablica[1] = 35;
+console.log(mojaTablica[1]);
+mojaTablica[2] = true;
+console.log(mojaTablica[2]);
+mojaTablica[3] = "hello";
+console.log(mojaTablica[3]);
+console.log(mojaTablica);
+mojaTablica[2] = false;
+console.log(mojaTablica[2]);
+console.log(mojaTablica);
+
+var mojaTablica2 = [10,20,"haj",false];
+console.log(mojaTablica2);
+
+var mojaTablica3 = new Array();
+var mojaTablica4 = new Array(5);
+
+console.log(mojaTablica2.length);
+console.log(mojaTablica2.sort());
+console.log(mojaTablica.sort());
+console.log(mojaTablica2.reverse());
+
+
+
+
+//-----------------------------------------------------------
+//epizod #25 kursu
+/*
 var str = "hello, world";
 console.log(str);
 var str2 = str.slice(2,9);
@@ -8,8 +41,10 @@ var tags = "tag, mięso, salami, kiełbasa, szynka";
 console.log(tags);
 var tagowTablica = tags.split(",");
 console.log(tagowTablica);
+*/
 
-
+//-----------------------------------------------------------
+//epizod #24 kursu
 /*
 var Stringi1 = "abc";
 var Stringi2 = "Bcd";
