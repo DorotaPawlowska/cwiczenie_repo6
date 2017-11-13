8bc4dcbf4caee2ee3bc19f68461569d7ea6ed169
Dorota Pawłowska <dpawlowska@op.pl>
2017-11-13 12:23:59 +0100

    commit nr33 JavaScript kurs1

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
+/*
+//-----------------------------------------------------------
+//#epizod 36 kursu
 
-
-
-
-
-
+var title = document.getElementById("page-title");
+title.setAttribute("style", "position: relative;");
+//title.setAttribute("style", "left: 10px;");
+title.setAttribute("style", "position: relative; left: 10px;");
+title.style.left = "20px";
+title.style.top = "10px";
+title.style.color = "red";
+title.style.backgroundColor = "blue";
+*/
 //-----------------------------------------------------------
 /*
 //#epizod 35 kursu
