8bfad3e287f8da80f4d551f4db3c263a5b50263e
Dorota Pawłowska <dpawlowska@op.pl>
2017-11-13 14:34:36 +0100

    commit nr37 JavaScript kurs1

--- test.js
+++ test.js
@@ -1,6 +1,19 @@
 //#epizod 40 kursu
 
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
 
+button.onclick = function () {
+    if(content.className == "open"){
+        // zmniejszam
+        content.className = "";
+        button.innerHTML = "Show More";
+    }else {
+        //rozszerzam
+        content.className = "open";
+        button.innerHTML = "Show Less";
+    }
+};
 
 
 
