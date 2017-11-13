fa8088443551c15df88adb046399109e27f74fad
Dorota Pawłowska <dpawlowska@op.pl>
2017-11-13 17:31:09 +0100

    commit nr42 JavaScript kurs1

--- test.js
+++ test.js
@@ -1,5 +1,20 @@
 //-----------------------------------------------------------
+/*
+//#epizod 45 kursu
 
+var myPara = document.getElementById("content").getElementsByTagName("p")[5];
+var myPara2 = $("#content p:last-child");
+
+var myPara2 = $("#content p:last-child");
+myPara2.addClass("test");
+myPara2.removeClass("test");
+myPara2.fadeOut();
+myPara2.fadeIn();
+myPara2.css({position: "relative", color: "red"});
+myPara2.animate({left: "50px"});
+*/
+//-----------------------------------------------------------
+/*
 //#epizod 44 kursu
 
 var myForm = document.forms.myForm;
@@ -14,7 +29,7 @@ myForm.onsubmit = function () {
         return true;
     }
 };
-
+*/
 
 //-----------------------------------------------------------
 /*
