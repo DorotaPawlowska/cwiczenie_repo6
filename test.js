2cd870dd9941ae6c0fe8fef7a9d67ca2d16f6899
Dorota Pawłowska <dpawlowska@op.pl>
2017-11-13 17:10:26 +0100

    commit nr41 JavaScript kurs1

--- test.js
+++ test.js
@@ -1,3 +1,20 @@
+//-----------------------------------------------------------
+
+//#epizod 44 kursu
+
+var myForm = document.forms.myForm;
+var message = document.getElementById("message");
+
+myForm.onsubmit = function () {
+    if (myForm.name.value == "") {
+        message.innerHTML = "please enter a name";
+        return false;
+    } else {
+        message.innerHTML = "";
+        return true;
+    }
+};
+
 
 //-----------------------------------------------------------
 /*
